2ad37d4ed545c8a360cf3492211abe0ea82bd11b
jbenjumead <jbenjumead@unal.edu.co>
2026-03-23 14:48:02 -0500

    version3
    Juan Camilo - Punto 3

--- PPPOOGrupo1/src/clases/Pelicula.java
+++ PPPOOGrupo1/src/clases/Pelicula.java
@@ -10,6 +10,8 @@ package clases;
  */
 public class Pelicula extends ContenidoMultimedia {
     String director;
+    public String artCalidad;
+    public String idiomaSubtitulos;
 
     public Pelicula(String director, String titulo, int duracionMinutos, int anioLanzamiento) {
         super(titulo, duracionMinutos, anioLanzamiento);
@@ -20,6 +22,14 @@ public class Pelicula extends ContenidoMultimedia {
         System.out.println("Cargabdo video y ajustando subtitulos para la pelicula...");
     }
     
-    
+    public void configurarVisualizacion(){
+        System.out.println("Reproduciendo en calidad Automatica, idioma original.");
+    }
+    public void configurarVisualizacion(String artCalidad){
+        System.out.println("Reproduciendo en calidad"+artCalidad);
+    }
+    public void configurarVisualizacion(String artCalidad, String idiomaSubtitulos){
+        System.out.println("Reproduciendo en calidad"+artCalidad+"con subtitulos en"+idiomaSubtitulos);
+    }
     
 }
